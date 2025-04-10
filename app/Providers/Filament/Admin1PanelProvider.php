e07d34b443a0c4624e64929998aa8bea7f7193bc
holahaell <holahaell@gmail.com>
2025-04-10 18:58:18 +0700

    feature: reimburse

--- app/Providers/Filament/Admin1PanelProvider.php
+++ app/Providers/Filament/Admin1PanelProvider.php
@@ -18,22 +18,22 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
 
-class AdminPanelProvider extends PanelProvider
+class Admin1PanelProvider extends PanelProvider
 {
     public function panel(Panel $panel): Panel
     {
         return $panel
-            ->id('admin')
-            ->path('admin')
+            ->id('admin1')
+            ->path('admin1')
             ->colors([
                 'primary' => Color::Amber,
             ])
-            ->discoverResources(in: app_path('Filament/Admin/Resources'), for: 'App\\Filament\\Admin\\Resources')
-            ->discoverPages(in: app_path('Filament/Admin/Pages'), for: 'App\\Filament\\Admin\\Pages')
+            ->discoverResources(in: app_path('Filament/Admin1/Resources'), for: 'App\\Filament\\Admin1\\Resources')
+            ->discoverPages(in: app_path('Filament/Admin1/Pages'), for: 'App\\Filament\\Admin1\\Pages')
             ->pages([
                 Pages\Dashboard::class,
             ])
-            ->discoverWidgets(in: app_path('Filament/Admin/Widgets'), for: 'App\\Filament\\Admin\\Widgets')
+            ->discoverWidgets(in: app_path('Filament/Admin1/Widgets'), for: 'App\\Filament\\Admin1\\Widgets')
             ->widgets([
                 Widgets\AccountWidget::class,
                 Widgets\FilamentInfoWidget::class,
